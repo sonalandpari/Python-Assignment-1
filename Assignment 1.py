e76f39e948eb50cc930fac448a0ac8eaedf008a4
Deep Dives <sonaljainstudy@gmail.com>
2025-07-16 11:59:04 +0530

    G:/Coding/Assignment

---
+++ Assignment 1.py
@@ -0,0 +1,15 @@
+# Task 1: Perform Basic Mathematical Operation
+
+a=int(input("Enter the First Number? "))
+b=int(input("Enter the Second Number? "))
+print("ADDITION:",a,"+",b,"=",a+b)
+print("SUBSTRACTION::",a,"-",b,"=",a-b)
+print("MULTIPLICATION:",a,"*",b,"=",a*b)
+print("DIVISION:",a,"/",b,"=",a/b)
+
+#TASK 2: Create a Personalised Greeting
+
+First_name=input("Enter your First name: ")
+Last_name=input("Enter your Last name: ")
+full_name= First_name +" "+ Last_name+"!"
+print("Hello,",full_name,"Welcome to Python Program")
